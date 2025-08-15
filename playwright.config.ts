7b73fe7e536fe059375078e7c41201b7612043d1
sohanjeet patra <sohanjeet.patra@fortunesoftit.com>
2025-08-15 17:16:44 +0530

    added allure report concept

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+   reporter: [
+    ['line'], // simple console output
+    ['allure-playwright'], // allure results
+    ['html'] // HTML report (optional)
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
